40d699722c58d16729889db485c17794bfdf3156
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-03-15 17:40:59 -0700

    lots of docs, generating properly

--- src/base.js
+++ src/base.js
@@ -10,22 +10,101 @@ const inspect = require('util').inspect;
 const tempfile = require('tempfile');
 const path = require('path');
 
-class Base {
+/**
+ * Extend your app from this class to get started.
+ *
+ *
+ * @example 
+ * // The following example is from {@link https://github.com/matrix-hacks/matrix-puppet-facebook|the facebook bridge}
+const {
+  MatrixAppServiceBridge: {
+    Cli, AppServiceRegistration
+  },
+  Puppet,
+  MatrixPuppetBridgeBase
+} = require("matrix-puppet-bridge");
+const FacebookClient = require('./client');
+const config = require('./config.json');
+const path = require('path');
+const puppet = new Puppet(path.join(__dirname, './config.json' ));
+const debug = require('debug')('matrix-puppet:facebook');
+
+class App extends MatrixPuppetBridgeBase {
+  getServicePrefix() {
+    return "facebook";
+  }
   initThirdPartyClient() {
-    throw new Error("override me");
+    this.threadInfo = {};
+    this.thirdPartyClient = new FacebookClient(this.config.facebook);
+    this.thirdPartyClient.on('message', (data)=>{
+      const { senderID, body, threadID, isGroup } = data;
+      const isMe = senderID === this.thirdPartyClient.userId;
+      this.threadInfo[threadID] = { isGroup };
+      const payload = {
+        roomId: threadID,
+        senderId: isMe ? undefined : senderID,
+        text: body
+      };
+      debug(payload);
+      return this.handleThirdPartyRoomMessage(payload);
+    });
+    return this.thirdPartyClient.login();
   }
-  getThirdPartyUserDataById(_thirdPartyUserId) {
-    throw new Error("override me and return or resolve a promise with at least {senderName: 'some name'}, otherwise provide it in the original payload and i will never be invoked");
+  getThirdPartyUserDataById(id) {
+    return this.thirdPartyClient.getUserInfoById(id).then(userInfo=>{
+      debug('got user data', userInfo);
+      return { senderName: userInfo.name };
+    });
   }
-  /**
-   * Async call to get additional data about the third party room
-   *
-   * @param {string} thirdPartyRoomId The unique identifier on the third party's side
-   * @returns {Promise->object} Promise resolving object { name:string, topic:string }
-   */
-  getThirdPartyRoomDataById(_thirdPartyRoomId) {
-    throw new Error("override me");
+  getThirdPartyRoomDataById(threadId) {
+    debug('getting third party room data by thread id', threadId);
+    let label = this.threadInfo[threadId].isGroup ? "Group" : "Friend";
+    return this.thirdPartyClient.getThreadInfo(threadId).then(data=>{
+      let roomData = {
+        name: data.name,
+        topic: `Facebook ${label}`
+      };
+      debug('room data', roomData);
+      return roomData;
+    });
   }
+  sendMessageAsPuppetToThirdPartyRoomWithId(id, text) {
+    return this.thirdPartyClient.sendMessage(id, text);
+  }
+}
+
+new Cli({
+  port: config.port,
+  registrationPath: config.registrationPath,
+  generateRegistration: function(reg, callback) {
+    puppet.associate().then(()=>{
+      reg.setId(AppServiceRegistration.generateToken());
+      reg.setHomeserverToken(AppServiceRegistration.generateToken());
+      reg.setAppServiceToken(AppServiceRegistration.generateToken());
+      reg.setSenderLocalpart("facebookbot");
+      reg.addRegexPattern("users", "@facebook_.*", true);
+      callback(reg);
+    }).catch(err=>{
+      console.error(err.message);
+      process.exit(-1);
+    });
+  },
+  run: function(port) {
+    const app = new App(config, puppet);
+    return puppet.startClient().then(()=>{
+      return app.initThirdPartyClient();
+    }).then(() => {
+      return app.bridge.run(port, config);
+    }).then(()=>{
+      console.log('Matrix-side listening on port %s', port);
+    }).catch(err=>{
+      console.error(err.message);
+      process.exit(-1);
+    });
+  }
+}).run();
+ */
+class Base {
   /**
    * The short string to put before the ghost user name.
    * e.g. return "groupme" for @groupme_bob:your.host.com
@@ -45,6 +124,7 @@ class Base {
   getServiceName() {
     throw new Error("override me");
   }
+
   /**
    * Return a user id to match against 3rd party user id's in order to know if the message is of self-origin
    *
@@ -53,10 +133,28 @@ class Base {
   getPuppetThirdPartyUserId() {
     throw new Error('override me');
   }
+
+  /**
+   * Implement how a text-based message is sent over the third party network
+   *
+   * @param {string} _thirdPartyRoomId
+   * @param {string} _messageText
+   * @param {object} _matrixEvent
+   * @returns {Promise}
+   */
   sendMessageAsPuppetToThirdPartyRoomWithId(_thirdPartyRoomId, _messageText, _matrixEvent) {
     throw new Error('override me');
   }
 
+  /**
+   * Implement how an image message is sent over the third party network
+   *
+   * @param {string} _thirdPartyRoomId
+   * @param {string} _messageText
+   * @param {string} _imageFile
+   * @param {object} _matrixEvent
+   * @returns {Promise}
+   */
   sendPictureMessageAsPuppetToThirdPartyRoomWithId(_thirdPartyRoomId, _messageText, _imageFile, _matrixEvent) {
     throw new Error('override me');
   }
@@ -77,6 +175,13 @@ class Base {
     return "puppetStatusRoom";
   }
 
+  /**
+   * @constructor
+   *
+   * @param {object} config Config as a JavaScript object
+   * @param {object} puppet Instance of Puppet to use
+   * @param {object} bridge Optional instance of Bridge to use
+   */
   constructor(config, puppet, bridge) {
     const { info } = debug();
     this.allowNullSenderName = false;
@@ -91,6 +196,32 @@ class Base {
     info('initialized');
   }
 
+  /**
+   * Optional async call to get additional data about the third party user, for when this information does not arrive in the original payload
+   *
+   * @param {string} thirdPartyRoomId The unique identifier on the third party's side
+   * @returns {Promise} Resolve with an object like {senderName: 'some name'}
+   */
+  getThirdPartyUserDataById(_thirdPartyUserId) {
+    throw new Error("override me and return or resolve a promise with at least {senderName: 'some name'}, otherwise provide it in the original payload and i will never be invoked");
+  }
+  /**
+   * Optional async call to get additional data about the third party room, for when this information does not arrive in the original payload
+   *
+   * @param {string} thirdPartyRoomId The unique identifier on the third party's side
+   * @returns {Promise} Resolve with an object like { name:string, topic:string }
+   */
+  getThirdPartyRoomDataById(_thirdPartyRoomId) {
+    throw new Error("override me");
+  }
+
+  /**
+   * Instantiates a Bridge for you. Called by the constructor if an existing bridge instance was not provided.
+   *
+   * @param {object} config bridge configuration (homeserverUrl, domain, registration)
+   *
+   * @private
+   */
   setupBridge(config) {
     return new Bridge(Object.assign({}, config.bridge, {
       controller: {
@@ -117,6 +248,14 @@ class Base {
       }
     }));
   }
+  /**
+   * Send a message to the status room
+   *
+   * @param {object} options={} Optional options object: fixedWidthOutput:boolean
+   * @param {string} ...args additional arguments are formatted and send to the room
+   *
+   * @returns {Promise}
+   */
   sendStatusMsg(options={}, ...args) {
     if (typeof options !== 'object') {
       throw new Error('sendStatusMsg requires first parameter to be an options object which can be empty.');
@@ -251,7 +390,7 @@ class Base {
    * provide a senderName when invoking handleThirdPartyRoomMessage
    *
    * @param {string} thirdPartyUserId
-   * @returns {Promise=>object}
+   * @returns {Promise}
    */
   getOrInitRemoteUserStoreDataFromThirdPartyUserId(thirdPartyUserId) {
     const { info } = debug(this.getOrInitRemoteUserStoreDataFromThirdPartyUserId.name);
@@ -320,9 +459,6 @@ class Base {
       });
     });
   }
-  /**
-   * Returns a promise
-   */
   handleThirdPartyRoomMessage(thirdPartyRoomMessageData, doNotTryToGetRemoteUserStoreData) {
     const { info } = debug(this.handleThirdPartyRoomMessage.name);
     info('handling third party room message', thirdPartyRoomMessageData);
