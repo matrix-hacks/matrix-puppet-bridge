a374c4ebf26555bfa84e9df2819a59a038aaf96f
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-03-21 14:50:50 -0700

    export utils

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   Puppet: require('./src/puppet'),
   MatrixPuppetBridgeBase: require('./src/base'),
   MatrixAppServiceBridge: require('matrix-appservice-bridge'),
-  MatrixSdk: require('matrix-js-sdk')
+  MatrixSdk: require('matrix-js-sdk'),
+  utils: require('./src/utils'),
 };
