1fb97d2ef2bd2ef49a908728b5fb854aa7ff34b9
Keyvan Fatehi <keyvanfatehi@gmail.com>
2017-05-29 16:57:59 -0700

    setup a v2 api experiment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   utils: require('./src/utils'),
   debug: require('./src/debug'),
   Puppet: require('./src/puppet'),
+  MatrixPuppetBridge: require('./src/v2'),
   MatrixPuppetBridgeBase: require('./src/base'),
   MatrixAppServiceBridge: require('matrix-appservice-bridge'),
   MatrixSdk: require('matrix-js-sdk'),
---
+++ src/v2/index.js
@@ -0,0 +1,3 @@
+function PuppetBridge() {}
+
+module.exports = PuppetBridge;
